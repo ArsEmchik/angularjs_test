a2bf3eabe153229cfae5684454f51096d830ca7a
ars <arsemchik@gmail.com>
2014-09-28 22:29:13 +0300

    validates length of name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :question
   validates :name, :answer, presence: true
+  validates_length_of :name, minimum: 1
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :name, presence: true
+  validates_length_of :name, minimum: 1
 end
